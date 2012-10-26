d3722dfe4cbc349f5c69de95daec04b99203f595
Cody Callahan <CallahanRTS@gmail.com>
2012-10-25 18:06:14 -0700

    Initial Commit

---
+++ CardWallet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardWallet
+//
+//  Created by Cody Callahan on 10/25/12.
+//  Copyright (c) 2012 RCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
